c65ee57585b037d73d96c25c263d99402e7a03bf
Adriaan Knapen <hi@aknapen.nl>
2024-10-30 13:03:56 +0100

    Implement webserver in exercisefiles/node folder
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Addono/CopilotHackathon?shareId=XXXX-XXXX-XXXX-XXXX).

--- exercisefiles/node/test.js
+++ exercisefiles/node/test.js
@@ -1,9 +1,3 @@
-//write npm command line to install mocha
-//npm install --global mocha
-
-//command to run this test file
-//mocha test.js
-
 const assert = require('assert');
 const http = require('http');
 const server = require('./nodeserver');
@@ -23,18 +17,101 @@ describe('Node Server', () => {
         });
     });
 
-    //add test to check get when key is equal to world
+    it('should return "hello world" if key is equal to "world"', (done) => {
+        http
+        .get('http://localhost:3000/get?key=world' , (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'hello world');
+                done();
+            });
+        });
+    });
 
-    //add test to check validatephoneNumber
+    it('should return "valid" if phone number is valid', (done) => {
+        http
+        .get('http://localhost:3000/Validatephonenumber?phoneNumber=+34666777888' , (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'valid');
+                done();
+            });
+        });
+    });
 
-    //write test to validate validateSpanishDNI
-   
+    it('should return "invalid" if phone number is invalid', (done) => {
+        http
+        .get('http://localhost:3000/Validatephonenumber?phoneNumber=123456789' , (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'invalid');
+                done();
+            });
+        });
+    });
 
-    //write test for returnColorCode red should return code #FF0000
+    it('should return "valid" if DNI is valid', (done) => {
+        http
+        .get('http://localhost:3000/ValidateSpanishDNI?dni=12345678Z' , (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'valid');
+                done();
+            });
+        });
+    });
 
+    it('should return "invalid" if DNI is invalid', (done) => {
+        http
+        .get('http://localhost:3000/ValidateSpanishDNI?dni=12345678A' , (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'invalid');
+                done();
+            });
+        });
+    });
 
-   //write test for daysBetweenDates
+    it('should return "#FF0000" if color is red', (done) => {
+        http
+        .get('http://localhost:3000/ReturnColorCode?color=red' , (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, '#FF0000');
+                done();
+            });
+        });
+    });
 
-
-
+    it('should return the number of days between two dates', (done) => {
+        http
+        .get('http://localhost:3000/DaysBetweenDates?date1=2023-01-01&date2=2023-01-10' , (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, '9 days');
+                done();
+            });
+        });
+    });
 });
